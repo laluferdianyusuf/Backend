137b2163786a78655ed8667847e945b1b3fd3792
laluferdianyusuf <lferdianyusuf@gmail.com>
2023-09-10 10:32:32 +0800

    sekian kali

--- config/config.js
+++ config/config.js
@@ -1,28 +1,32 @@
 require("dotenv").config();
 const Sequelize = require("sequelize");
-const db = new Sequelize(process.env.DB_URL, {
+
+const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_URL } = process.env;
+
+const db = new Sequelize(DB_URL, {
   define: {
     timestamp: false,
   },
 });
+
 module.exports = {
   db,
-  development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.PASSWORD,
-    database: process.env.DB_NAME,
-    dialect: "postgres",
-  },
-  test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.PASSWORD,
-    database: process.env.DB_NAME,
-    dialect: "postgres",
-  },
-  production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.PASSWORD,
-    database: process.env.DB_NAME,
-    dialect: "postgres",
-  },
+  // development: {
+  //   username: process.env.DB_USERNAME,
+  //   password: process.env.PASSWORD,
+  //   database: process.env.DB_NAME,
+  //   dialect: "postgres",
+  // },
+  // test: {
+  //   username: process.env.DB_USERNAME,
+  //   password: process.env.PASSWORD,
+  //   database: process.env.DB_NAME,
+  //   dialect: "postgres",
+  // },
+  // production: {
+  //   username: process.env.DB_USERNAME,
+  //   password: process.env.PASSWORD,
+  //   database: process.env.DB_NAME,
+  //   dialect: "postgres",
+  // },
 };
