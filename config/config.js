3873ffbf6215fbd83d22e27d969a77243976a3dd
laluferdianyusuf <lferdianyusuf@gmail.com>
2023-09-10 09:54:48 +0800

    seventh commit

--- config/config.js
+++ config/config.js
@@ -1,23 +1,37 @@
-module.exports = {
-  development: {
-    username: "postgres",
-    password: "aUBlAO6llwgijOa1Ypsh",
-    database: "railway",
-    host: "containers-us-west-140.railway.app",
-    dialect: "postgres",
+const dotenv = require("dotenv");
+const Sequelize = require("sequelize");
+
+dotenv.config();
+
+const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, DB_URL } = process.env;
+
+const db = new Sequelize(DB_URL, {
+  define: {
+    timestamp: false,
   },
-  test: {
-    username: "postgres",
-    password: "aUBlAO6llwgijOa1Ypsh",
-    database: "railway",
-    host: "containers-us-west-140.railway.app",
-    dialect: "postgres",
-  },
-  production: {
-    username: "postgres",
-    password: "aUBlAO6llwgijOa1Ypsh",
-    database: "railway",
-    host: "containers-us-west-140.railway.app",
-    dialect: "postgres",
-  },
-};
+});
+
+module.exports = db;
+// module.exports = {
+//   development: {
+//     username: "postgres",
+//     password: "aUBlAO6llwgijOa1Ypsh",
+//     database: "railway",
+//     host: "containers-us-west-140.railway.app",
+//     dialect: "postgres",
+//   },
+//   test: {
+//     username: "postgres",
+//     password: "aUBlAO6llwgijOa1Ypsh",
+//     database: "railway",
+//     host: "containers-us-west-140.railway.app",
+//     dialect: "postgres",
+//   },
+//   production: {
+//     username: "postgres",
+//     password: "aUBlAO6llwgijOa1Ypsh",
+//     database: "railway",
+//     host: "containers-us-west-140.railway.app",
+//     dialect: "postgres",
+//   },
+// };
